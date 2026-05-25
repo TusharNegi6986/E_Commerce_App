53cb8e0c1e507ab3704a88f2fc46578e60e3fca3
Sunny <tusharnegi457@gmail.com>
2026-05-25 16:41:26 +0530

    fix cors

--- backend/server.js
+++ backend/server.js
@@ -1,54 +1,45 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
-import connectDB from "./config/mongodb.js";
-import connectCloudinary from "./config/cloudinary.js";
-import userRouter from "./routes/userRoute.js";
-import productRouter from "./routes/productRoute.js";
-import cartRouter from "./routes/cartRoute.js";
-import orderRouter from "./routes/orderRoute.js";
 
 const app = express();
 
-// DB + Cloudinary
-connectDB();
-connectCloudinary();
+app.use(express.json());
 
 const allowedOrigins = [
   "http://localhost:5173",
   "http://127.0.0.1:5173",
+
+  // user frontend
   "https://e-commerce-app-frontend-red.vercel.app",
 
-  // admin panel
+  // admin frontend
   "https://e-commerce-app-admin-gamma.vercel.app"
 ];
 
-// CORS
 app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin) return callback(null, true);
+  origin: (origin, callback) => {
+
+    console.log("Request origin:", origin);
+
+    if (!origin) {
+      return callback(null, true);
+    }
 
     if (allowedOrigins.includes(origin)) {
       return callback(null, true);
     }
 
-    return callback(null, false);
+    console.log("Blocked Origin:", origin);
+
+    return callback(new Error("CORS not allowed"));
   },
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+
   credentials: true,
-  allowedHeaders: ["Content-Type", "Authorization", "token"]
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: [
+    "Content-Type",
+    "Authorization",
+    "token"
+  ]
 }));
-
-app.use(express.json());
-
-// routes
-app.use("/api/user", userRouter);
-app.use("/api/product", productRouter);
-app.use("/api/cart", cartRouter);
-app.use("/api/order", orderRouter);
-
-app.get("/", (req, res) => {
-  res.send("API Working");
-});
-
-export default app;
